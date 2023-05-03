6f244bdaa84adb28dbcb94aa5abe6c24ce9752c3
lucangelsolis <lucangel7@gmail.com>
2023-05-03 14:47:36 -0300

    proyectojsx

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
 
+function retornarAleatorio() {
+  return Math.trunc(Math.random() * 10);
+}
+
 function App() {
+  const siglo = 21
+  const persona = {
+    nombre: 'Juan',
+    edad: 34
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Título nivel 1</h1>
+      <hr />
+      <p>Estamos en el siglo {siglo}</p>
+      <h3>Acceso a un objeto</h3>
+      <p>{persona.nombre} tiene {persona.edad} años</p>
+      <h3>Llamada a un método</h3>
+      <p>Un valor aleatorio llamando a un método.</p>
+      {retornarAleatorio()}
+      <h3>Calculo inmediato de expresiones</h3>
+      3 + 3 = {3 + 3}
     </div>
   );
 }
